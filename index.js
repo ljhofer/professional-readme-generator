daf76cc8f37239604102ec98d6282088d7c33aee
Laura Hofer <ljhofer@gmail.com>
2021-10-12 06:44:48 -0500

    Added to template string and prompt/then language to init function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -8,68 +9,88 @@ const questions = [
         type: "input",
         message: "What is your project's title?",
         name: "title"
-
     },
     {
         type: "input",
         message: "Describe your project",
         name: "description"
-
     },
     {
         type: "input",
         message: "What are the installation requirements?",
         name: "installation"
-
     },
     {
         type: "input",
         message: "What is the usage for this project?",
         name: "usage"
-
     },
     {
         type: "list",
         message: "Which license would you like?",
         name: "license",
-        choices: ['MIT', 'Apache', 'GNU General Public License', 'Mozilla Public License 2.0'],
-
+        choices: ["MIT", "Apache", "GNU General Public License", "Mozilla Public License 2.0", "I don't want a license"],
     },
     {
         type: "input",
         message: "What are the contributing guidelines for the project?",
         name: "contributing"
-
     },
     {
         type: "input",
         message: "What are some examples of tests for your project?",
         name: "tests"
-
     },
     {
         type: "input",
         message: "What is your GitHub username?",
         name: "gitHub"
-
     },
     {
         type: "input",
         message: "What is your email address?",
         name: "email"
-
     },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, finalReadme) {
+
+    // TODO: can We write to this readme or do we create a new one with a different file?
+    fs.writeFile(fileName, finalReadme, err => {
+        err ? console.log("Oops!") : console.log("Nice!")
+    })
+
+
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-        // .prompt(questions)
-        // .then
+        .prompt(questions)
+        // TODO: deal with response pass to write file 
+        .then ((responses) => {
+            console.log(responses);
+            
+            //TODO: Save project title to a variable
+            const {fileName} = responses.title + ".md";
 
+
+
+            //const finalReadme = generateMarkdown(responses)
+           
+
+
+        
+        //writeToFile(fileName, finalReadme);
+
+
+
+
+
+
+
+        })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,50 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // TODO: Add switch statements for these
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+  // TODO: Add switch statements for these
+  //TODO: Start with just the link with the text of license title, later replace with license badge
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+// TODO: Figure out how to do the Table of Contents section and make it link
+// TODO: Make the GH link actually link
+// TODO: Add in licensing stuff
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description 
+  ${data.description}
+  
+  ## Table of Contents
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Please eamil any questions to to ${data.email} or reference my GitHub profile at https://github.com/${data.gitHub}. 
+
+  ##Licensing
 
 `;
 }
