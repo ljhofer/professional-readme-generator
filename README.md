2a16cf12a9e32ee863ef11dc6b47ffea8aa2e767
Laura Hofer <ljhofer@gmail.com>
2021-10-13 08:21:42 -0500

    Added link to badge and added content to README including screen shots of usage

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# professional-readme-generator
+
+# Professional README Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 Bootcamp Homework #9
+
+## Description 
+I have created a ccommand line application that allows users to create a professional README markdown file to include with their project's documentation. It creates common README sections like description, usage, contributions, and license information. Additionally, the program allows them to include a responsive table of contents. 
+    
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributions](#contributions)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+This application requires Node and users should run "npm install" in their command line prior to use. 
+
+## Usage
+After installing Node and running "npm install" users will open their command line and initiate the program by typing "node index.js". 
+![Picture of inital command line entry](./assets/images/commandline.png)
+
+Then answer the questions with the information you would like included in your README. 
+![Picture of first question in the command line](./assets/images/questions.png)
+
+After entering all of your information, the program will create a README file in your folder. 
+
+Here is a screencast of how the program runs. 
+
+[Video of creator using the program](https://watch.screencastify.com/v/jm1b2txhnvXwFx7ktnXj)
+![Video of creator using the program](./assets/images/testvideo.mp4)
+
+
+
+
+## License
+This project licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+## Contributions
+Not accepting contributions
+
+## Tests
+Project runs with expected input. 
+
+## Questions
+Please email any questions to <ljhofer@gmail.com> or reference my [GitHub profile](https://github.com/ljhofer). 
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,24 @@
-// TODO: decide about license section and ammend these comments
-// Creates a function that returns a license badge based on which license is passed in, if no license is selected an empty string will be returned
+
+// Returns a license badge based on which license is passed in, if no license is selected an empty string will be returned
 function renderLicenseBadge(license) {
 
     let licenseBadge = "";    
 
     switch(license) {
         case "MIT":
-            licenseBadge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
             break;
 
         case "Apache":
-            licenseBadge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
             break;
             
         case "Boost Software License": 
-            licenseBadge = "![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)";
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
             break;
 
         case "Mozilla Public License 2.0":
-            licenseBadge = "![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)";
+            licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
             break;
     }
     return licenseBadge;   
@@ -30,19 +30,19 @@ function renderLicenseLink(license) {
 
     switch(license) {
         case "MIT":
-            licenseLink = "This project licensed under the [MIT License]](https://opensource.org/licenses/MIT)";
+            licenseLink = "This project licensed under the [MIT License](https://opensource.org/licenses/MIT).";
             break;
 
         case "Apache":
-            licenseLink = "This project licensed under the [Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)";
+            licenseLink = "This project licensed under the [Apache 2.0 License](https://opensource.org/licenses/Apache-2.0).";
             break;
         
         case "Boost Software License": 
-            licenseLink = "This project licensed under the [Boost Software License](https://www.boost.org/LICENSE_1_0.txt)";
+            licenseLink = "This project licensed under the [Boost Software License](https://www.boost.org/LICENSE_1_0.txt).";
             break;
 
         case "Mozilla Public License 2.0":
-            licenseLink = "This project licensed under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)";
+            licenseLink = "This project licensed under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0).";
             break;
 
         case "I don't want a license":
@@ -50,18 +50,6 @@ function renderLicenseLink(license) {
             break;
     }     
     return licenseLink;
-}
-
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-
-
-  //TODO: pass license info to the generateMarkdown function somwehow
-
-
-
 }
 
 // Generates the template string to be passed to the markdown file
@@ -72,11 +60,8 @@ function generateMarkdown(data) {
 
    
     return `# ${data.title}
-## License
-${licenseLink}
 ${licenseBadge}
 
-    
 ## Description 
 ${data.description}
     
@@ -84,6 +69,7 @@ ${data.description}
 
 - [Installation](#installation)
 - [Usage](#usage)
+- [License](#license)
 - [Contributions](#contributions)
 - [Tests](#tests)
 - [Questions](#questions)
@@ -94,6 +80,9 @@ ${data.installation}
 ## Usage
 ${data.usage}
 
+## License
+${licenseLink}
+
 ## Contributions
 ${data.contributing}
 
@@ -101,7 +90,7 @@ ${data.contributing}
 ${data.tests}
 
 ## Questions
-Please email any questions to ${data.email} or reference my [GitHub profile](https://github.com/${data.gitHub}). 
+Please email any questions to <${data.email}> or reference my [GitHub profile](https://github.com/${data.gitHub}). 
 `;
 }
 
